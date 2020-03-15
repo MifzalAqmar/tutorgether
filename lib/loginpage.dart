2be42485ebeda2c20c24aa72cf52ec18257604d4
MifzalAqmar <mifzal.aqmar@gmail.com>
2020-03-15 21:54:09 +0800

    Initial commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:tutorgether/registeruser.dart';
+ 
+void main() => runApp(LoginPage());
+ 
+ bool _isChecked = true;
+ final TextEditingController _emcontroller = TextEditingController();
+  String _email = "";
+  final TextEditingController _pscontroller = TextEditingController();
+  String _pass = "";
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: new Container(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/TutorGether.png',
+              scale: 0.5,
+            ),
+            TextField(
+              controller: _emcontroller,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              )),
+              TextField(
+                controller: _pscontroller,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                ),
+                Row(
+                  children: <Widget>[
+                    Checkbox(
+                      value: _isChecked,
+                      onChanged: (bool value) {
+                        _onChange(value);
+                      },
+                      ),
+                      Text('Remember Me', style: TextStyle(fontSize: 15)) 
+                  ],),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                      minWidth: 300,
+                      height: 50,
+                      child: Text('Login'),
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      elevation: 20,
+                      onPressed: _onPress,
+
+
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: _onRegister,
+                      child: Text("Register new account"),
+                      ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('Forgot password'),
+                  
+          ],)
+      )
+    );
+
+  }
+  void _onPress() {
+    print(_pscontroller);
+    print(_emcontroller);
+  }
+  void _onChange(bool value) {
+    setState(() {
+      _isChecked = value;
+      print('Check value $value');
+    });
+  }
+  void _onRegister() {
+    Navigator.push(
+      context, MaterialPageRoute(builder: (context) => RegisterUser()));
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:tutorgether/loginpage.dart';
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blueGrey),
+      debugShowCheckedModeBanner: false,
+      
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/TutorGether.png',
+                scale: 0.5,
+              ),
+              SizedBox(
+                height: 20,
+                ),
+                new ProgressIndicator(),
+            ],
+
+          )
+          ),
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        
+      ),
+    );
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  @override
+  _ProgressIndicatorState createState() => new _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator> 
+  with SingleTickerProviderStateMixin {
+    AnimationController controller;
+    Animation<double> animation;
+
+    @override
+    void initState() {
+      super.initState();
+      controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+        animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+        ..addListener((){
+          setState(() { 
+          if (animation.value > 0.99){
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => LoginPage()));
+            
+          }});});
+          controller.repeat();
+        }
+
+        @override
+        void dispose(){
+          controller.stop();
+          super.dispose();
+        }
+
+        @override 
+        Widget build(BuildContext context) {
+          return new Center(
+            child: new Container(
+            width: 200,
+            color: Colors.blueAccent,
+            child: LinearProgressIndicator(
+              value: animation.value,
+              backgroundColor: Colors.black,
+              valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
+
+            ),
+            )
+          );
+        }
+
+      
+      
+    }
+  
